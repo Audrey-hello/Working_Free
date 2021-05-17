9fbbdd76a52df018a6bfb4811fc03de41440ca22
Audrey Le Moine <audreylm33@gmail.com>
2021-05-17 17:32:09 +0200

    ok

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,19 +1,28 @@
 class OfficesController < ApplicationController
-
+before_action :set_office, only: [:show, :destroy]
   def index
-
+    @offices = Office.all
   end
 
   def new
-
+    @office = Office.new
   end
 
   def create
-
+     @office = Office.new(office_params)
+    if @office.save
+      redirect_to office_path(@office)
+    else
+      render :new
+    end
   end
 
   def show
-
+   @bookings = Booking.new
+   @office = Office.new(office: @office)
   end
 
+  def set_office
+    @office = Office.find(params[:id])
+  end
 end
