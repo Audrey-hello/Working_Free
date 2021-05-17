37a1d8813a028f0d1df75d8cd21728d2349ac8b1
Mathieu Chaplain <mathieuchaplain@gmail.com>
2021-05-17 17:40:30 +0200

    creation fonction index de offices

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,7 +1,14 @@
 class OfficesController < ApplicationController
-
+before_action :set_office, only: [:show, :edit, :update, :destroy]
   def index
-
+        if params[:query].present?
+      @query = params[:query]
+      @offices = Office.where("name LIKE ?","%#{params[:query]}%")
+      # Preventing SQL Injection and Database error for
+      # unknown characters
+    else
+      @offices = Office.all
+    end
   end
 
   def new
@@ -15,5 +22,16 @@ class OfficesController < ApplicationController
   def show
 
   end
+
+  private
+
+  def set_office
+    @office = Office.find(params[:id])
+  end
+
+  def office_params
+    params.require(:office).permit(:name, :address, :description, :price, :capacity, :picture_url)
+  end
+end
 
 end
