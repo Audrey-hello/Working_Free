47c9f9e49cd2c5e5f08a060a7ff958acd35d8260
Mathieu Chaplain <mathieuchaplain@gmail.com>
2021-05-18 13:04:44 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user_1 = User.create(email: user_1@gmail.com, password: '123456')
-user_2 = User.create(email: user_2@gmail.com, password: '456789')
+user_1 = User.create(email: 'user_1@gmail.com', password: '123456')
+user_2 = User.create(email: 'user_2@gmail.com', password: '456789')
 
 
   offices = [{ adress: '14 rue Crespin du Gast', title: 'Le Wagon', price: '25€', description: '60 workstations in openspace', user: user_1},{ adress: '16 villa gaudelet ,75011 Paris', title: 'Le Wagon', price: '25€', description: '90 workstations in openspace', user: user_1},{ adress: '64-66 rue des archives ,75003 Paris', title: 'wework coeur marais', price: '40€', description: '150 workstations in openspace', user: user_2},{ adress: 'tour saint jacques ,750 Paris', title: 'openspace', price: '35€', description: '60 workstations in openspace', user: user_2},{ adress: 'Rue du Cloître Saint-Merri ,75004 Paris', title: 'workingflex', price: '20€', description: '20 workstations in openspace', user: user_2}]
