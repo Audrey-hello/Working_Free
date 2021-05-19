64c1158f3709cb1b7df8d5c5c0344f7e5ec7c3f7
Jiwon EUN <jiwon.eun@gmail.com>
2021-05-19 11:45:08 +0200

    changement

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,6 +1,7 @@
 class Office < ApplicationRecord
   belongs_to :user
-  has_many_attached :photo
+  has_many_attached :photos
+  has_many :bookings
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
