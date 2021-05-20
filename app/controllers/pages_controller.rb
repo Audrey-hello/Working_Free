01eda9c5d7bb377b118ff78b6282510c72a75709
Mathieu Chaplain <mathieuchaplain@gmail.com>
2021-05-20 14:53:54 +0200

    pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
   def home
   end
+
+  def dashboard
+    @offices = Office.where(user: current_user)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
 class UsersController < ApplicationController
 
-
 end
