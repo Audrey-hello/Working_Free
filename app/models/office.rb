ad0af4bca7b62811732d7ba177e7c8d61a93ba4d
Anne Lepetit <lepetit.anne@gmail.com>
2021-05-20 17:34:01 +0200

    search-bar all cases

--- app/models/office.rb
+++ app/models/office.rb
@@ -4,4 +4,12 @@ class Office < ApplicationRecord
   has_many :bookings
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+    pg_search_scope :global_search,
+      against: [ :address, :description, :title ],
+      using: {
+        tsearch: { any_word: true }
+      }
+
 end
