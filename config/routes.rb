0f8fa7baf090bfb65f1f936fd409922f704842b7
Mathieu Chaplain <mathieuchaplain@gmail.com>
2021-05-20 12:23:38 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offices, only: [ :index, :show, :create, :new ] do
-    resources :bookings, only: [ :new, :create ]
+    resources :bookings, only: [ :new, :create ] do
+      resources :dashboards, only: [ :new, :create ]
   end
 resources :bookings, only: [ :edit, :update ]
+resources :dashboards, only: [ :edit, :update ]
 end
