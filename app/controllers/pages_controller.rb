e3bf1cca1145929040c3a24409bed5af99d00001
Mathieu Chaplain <mathieuchaplain@gmail.com>
2021-05-21 10:58:26 +0200

    update last merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,13 @@ class PagesController < ApplicationController
   def home
 
   end
+
+  def dashboard
+    @offices = Office.where(user: current_user)
+    @bookings = Booking.where(user: current_user)
+  end
+
+
+
+
 end
