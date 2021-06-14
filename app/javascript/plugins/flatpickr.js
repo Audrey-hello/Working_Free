f98916322a8df41bbc42d09b4d49a9989dd6c67d
Audrey Le Moine <audreylm33@gmail.com>
2021-06-14 11:39:49 +0200

    dashboard

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -12,7 +12,7 @@ const initFlatpickr = () => {
   });
 
 }
-initFlatpickr();
+
 
 
 export { initFlatpickr };
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :offices
-  has_many :bookings
+  has_many :offices, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_many :requests, through: :offices, source: :bookings
 
   devise :database_authenticatable, :registerable,
